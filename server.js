54333da0f6488a6a757c2680838c73be949d99d4
EIT-NLP <xyshen@eitech.edu.cn>
2024-10-13 18:17:53 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/index-zh', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index-zh.html'));
+});
+
 app.get('/research', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'research.html'));
 });
@@ -21,6 +25,20 @@ app.get('/team', (req, res) => {
 
 app.get('/recruitment', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'recruitment.html'));
+});
+
+// 在现有路由之后添加以下内容
+
+app.get('/research-zh', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'research-zh.html'));
+});
+
+app.get('/team-zh', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'team-zh.html'));
+});
+
+app.get('/recruitment-zh', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'recruitment-zh.html'));
 });
 
 // 处理其他所有路由
